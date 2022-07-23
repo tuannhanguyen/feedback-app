655f318c4b75ad7ee74db984fa06e468f4339fd9
tuannhan <tuannhan6@gmail.com>
2022-07-23 11:16:01 +0700

    Real time validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -2,13 +2,28 @@ import React from 'react'
 import Card from './shared/Card'
 import Button from './shared/Button'
 import { useState } from 'react'
+import { FaCommentsDollar } from 'react-icons/fa'
 
 function FeedbackForm() {
 
     const [text, setText] = useState('')
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
     const handleTextChange = (e) => {
+        if (text === '') {
+            setMessage(null)
+            setBtnDisabled(true)
+        } else if (text !== '' && text.trim().length <= 10) {
+            setBtnDisabled(true)
+            setMessage('Text must be at least 10 characters')
+        } else {
+            setMessage(null)
+            setBtnDisabled(false)
+        }
+
         setText(e.target.value)
     }
+    
 
   return (
     <Card>
@@ -16,8 +31,9 @@ function FeedbackForm() {
         <h2>How would you rate your service with us ?</h2>
         <div className='input-group'>
             <input type="text" onChange={handleTextChange} value={text} placeholder='Write a review' />
-            <Button type='submit'>Send</Button>
+            <Button type='submit' isDisable={btnDisabled}>Send</Button>
         </div>
+        {message && <div>{message}</div>}
       </form>
     </Card>
   )
