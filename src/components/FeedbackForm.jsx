e30cce680254df2fed3db96cc5a65382fb5f6f13
tuannhan <tuannhan6@gmail.com>
2022-09-17 00:36:58 +0700

    11

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,13 +1,7 @@
 import React from 'react'
-import Card from './shared/Card'
 import CardMain from './shared/CardMain'
-import Button from './shared/Button'
-import { useState, useContext, useEffect } from 'react'
-import RatingSelect from './RatingSelect'
-import FeedbackContext from '../context/FeedbackContext'
 import happy from '../images/hasher-happy-sticker.gif'
 import iloveu from '../images/download.png'
-import img3 from '../images/giphy.gif'
 
 function FeedbackForm() {
 
@@ -18,7 +12,7 @@ function FeedbackForm() {
         <h2>CHÚC MỪNG SINH NHẬT</h2>
         <h2>"Cô bạn thân" MỸ LINH</h2>
         <h2>18.09.2022</h2>
-        <img src={happy} alt="" />
+        <img src={happy}/>
         <img src={iloveu}  />
       </form>
     </CardMain>
